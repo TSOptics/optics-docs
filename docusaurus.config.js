f6295507ea02fe202b1d56b4071a767e1cacf101
Vincent Quillien <vincent.quillien@dougs.fr>
2022-07-29 11:29:15 +0200

    Add guide for BaseOptic

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,116 +1,111 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-    title: 'React-Optics',
-    tagline: 'Composable state management for react',
-    url: 'https://react-optics.org',
-    baseUrl: '/',
-    onBrokenLinks: 'warn',
-    onBrokenMarkdownLinks: 'warn',
-    favicon: 'img/favicon/favicon.ico',
-    organizationName: 'TSOptics', // Usually your GitHub org/user name.
-    projectName: 'react-optics-docs', // Usually your repo name.
-    trailingSlash: false,
-    themeConfig: {
-        navbar: {
-            title: 'React-Optics',
-            logo: {
-                src: 'img/react-optics.svg',
-                srcDark: 'img/react-optics-dark.svg'
-            },
-            items: [
-                {
-                    type: 'doc',
-                    docId: 'Introduction/getting-started',
-                    position: 'left',
-                    label: 'Docs'
-                },
-                { to: '/blog', label: 'Blog', position: 'left' },
-                {
-                    href: 'https://github.com/TSOptics/react-optics',
-                    label: 'GitHub',
-                    position: 'right'
-                }
-            ]
+  title: "React-Optics",
+  tagline: "Composable state management for react",
+  url: "https://tsoptics.github.io",
+  baseUrl: "/",
+  onBrokenLinks: "warn",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon/favicon.ico",
+  organizationName: "TSOptics", // Usually your GitHub org/user name.
+  projectName: "react-optics-docs", // Usually your repo name.
+  trailingSlash: false,
+  themeConfig: {
+    navbar: {
+      title: "React-Optics",
+      logo: {
+        src: "img/react-optics.svg",
+        srcDark: "img/react-optics-dark.svg",
+      },
+      items: [
+        {
+          type: "doc",
+          docId: "Introduction/getting-started",
+          position: "left",
+          label: "Docs",
         },
-        footer: {
-            style: 'light',
-            links: [
-                {
-                    title: 'Docs',
-                    items: [
-                        {
-                            label: 'Tutorial',
-                            to: '/docs/Introduction/getting-started'
-                        }
-                    ]
-                },
-                {
-                    title: 'Community',
-                    items: [
-                        {
-                            label: 'Stack Overflow',
-                            href: 'https://stackoverflow.com/questions/tagged/docusaurus'
-                        },
-                        {
-                            label: 'Discord',
-                            href: 'https://discordapp.com/invite/docusaurus'
-                        },
-                        {
-                            label: 'Twitter',
-                            href: 'https://twitter.com/docusaurus'
-                        }
-                    ]
-                },
-                {
-                    title: 'More',
-                    items: [
-                        {
-                            label: 'Blog',
-                            to: '/blog'
-                        },
-                        {
-                            label: 'GitHub',
-                            href: 'https://github.com/TSOptics/react-optics'
-                        }
-                    ]
-                }
-            ],
-            copyright: `Copyright © ${new Date().getFullYear()} Vincent Quillien, Built with Docusaurus.`
+        { to: "/blog", label: "Blog", position: "left" },
+        {
+          href: "https://github.com/TSOptics/react-optics",
+          label: "GitHub",
+          position: "right",
         },
-        prism: {
-            theme: lightCodeTheme,
-            darkTheme: darkCodeTheme
-        }
+      ],
     },
-    presets: [
-        [
-            '@docusaurus/preset-classic',
+    footer: {
+      style: "light",
+      links: [
+        {
+          title: "Docs",
+          items: [
             {
-                docs: {
-                    sidebarPath: require.resolve('./sidebars.js'),
-                    // Please change this to your repo.
-                    editUrl:
-                        'https://github.com/TSOptics/react-optics-docs/edit/master',
-                    remarkPlugins: [
-                        [
-                            require('@docusaurus/remark-plugin-npm2yarn'),
-                            { sync: true }
-                        ]
-                    ]
-                },
-                blog: {
-                    showReadingTime: true,
-                    // Please change this to your repo.
-                    editUrl:
-                        'https://github.com/TSOptics/react-optics-docs/edit/master/'
-                },
-                theme: {
-                    customCss: require.resolve('./src/css/custom.css')
-                }
-            }
-        ]
-    ]
+              label: "Tutorial",
+              to: "/docs/Introduction/getting-started",
+            },
+          ],
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "Stack Overflow",
+              href: "https://stackoverflow.com/questions/tagged/docusaurus",
+            },
+            {
+              label: "Discord",
+              href: "https://discordapp.com/invite/docusaurus",
+            },
+            {
+              label: "Twitter",
+              href: "https://twitter.com/docusaurus",
+            },
+          ],
+        },
+        {
+          title: "More",
+          items: [
+            {
+              label: "Blog",
+              to: "/blog",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/TSOptics/react-optics",
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Vincent Quillien, Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl: "https://github.com/TSOptics/react-optics-docs/edit/master",
+          remarkPlugins: [
+            [require("@docusaurus/remark-plugin-npm2yarn"), { sync: true }],
+          ],
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl: "https://github.com/TSOptics/react-optics-docs/edit/master/",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
 };
